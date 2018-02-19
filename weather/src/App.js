7f8f83c1f58b368df2140105de5fed4d47440441
Robert Rollner <robrollner@gmail.com>
2018-02-19 13:02:04 -0700

    added axios

--- weather/src/App.js
+++ weather/src/App.js
@@ -2,8 +2,20 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Input from './components/Input';
 import Forecast from './components/Forecast';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+
+  
   render() {
     return (
       <div className="container-fluid">
--- weather/src/components/Forecast/Forecast.js
+++ weather/src/components/Forecast/Forecast.js
@@ -4,6 +4,7 @@ import "./Forecast.css";
 const Forecast = props => 
     <div>
         <h3>This week's forecast:</h3>
+        <div>{props.week}</div>
     </div>
 
 export default Forecast;
